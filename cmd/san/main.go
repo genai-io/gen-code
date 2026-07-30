1ea7dbc4be2da406f31c5c27dc63d2cfdc622527
Meng Yan <yanmxa@gmail.com>
2026-07-30 22:38:44 +0800

    chore: bump version to 1.22.1
    Signed-off-by: Meng Yan <yanmxa@gmail.com>

--- cmd/san/main.go
+++ cmd/san/main.go
@@ -33,7 +33,7 @@ import (
 	_ "github.com/genai-io/san/internal/llm/volcengine"
 )
 
-var version = "1.22.0"
+var version = "1.22.1"
 
 // buildTime and commit are set at build time via -X ldflags.
 // When built directly with go build (without ldflags), they remain empty.
